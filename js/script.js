fe0501c41e68b9b81f75e1ebaa9011b59664e4c7
robson <robsonmesquita-2011@outlook.com>
2022-05-10 19:45:08 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function mandazap (elemento) {
+    let texto = elemento.firstElementChild.innerText;
+    let numero = prompt("Qual é o número?")
+    let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${texto}`
+
+    window.open(zapLink)
+}
